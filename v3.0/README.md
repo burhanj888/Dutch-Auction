3f6a254282c91a90b167cc6da7708c0bf6c49e18
burhanj888 <burhanj888@gmail.com>
2023-02-23 21:23:22 -0500

    Dutch Auction V3.0 complete

--- v3.0/README.md
+++ v3.0/README.md
@@ -11,3 +11,78 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat run scripts/deploy.ts
 ```
+
+npx hardhat coverage
+
+Version
+=======
+> solidity-coverage: v0.8.0
+
+Instrumenting for coverage...
+=============================
+
+> MyERC20BidToken.sol
+> MyERC721NFT.sol
+> NFTDutchAuction_ERC20Bids.sol
+
+Compilation:
+============
+
+Nothing to compile
+No need to generate any newer typings.
+
+Network Info
+============
+> HardhatEVM: v2.11.1
+> network:    hardhat
+
+
+
+  Test Cases
+    Mint
+      ✔ ERC20 Tests - Cap Assertion (2282ms)
+
+  ERC20 and ERC721 Tests
+    ✔ ERC20 minting by Owner
+    ✔ ERC20 minting by Other Account
+    ✔ ERC20 - Cap exceeded (65ms)
+    ✔ ERC 721 - should revert when maximum token supply is reached (150ms)
+    Auction Tests
+      ✔ ERC721 - Safe Mint by Owner (38ms)
+      ✔ ERC721 - Safe Mint by Other Account
+      ✔ Deploy Dutch Auction Contract (237ms)
+
+  Bids
+    ✔ Pre ERC20 Approval
+    ✔ ERC20 Approved
+
+  After approval
+    ✔ should revert when the seller is trying to bid
+    ✔ should revert when the token is not approved (81ms)
+    ✔ should revert when the token id doesn't exist while approving
+    ✔ should revert when the approve caller is not the token owner
+    ✔ Approved
+
+  Bid After Approval
+    ✔ should rever when the bid placed is less than current price (41ms)
+    ✔ Successful Bid
+    ✔ should revert when a bid is placed after auction is closed
+
+  Transfer to seller
+    ✔ should transfer tokens from highestBidder to seller on successful bid
+
+
+  19 passing (3s)
+
+--------------------------------|----------|----------|----------|----------|----------------|
+File                            |  % Stmts | % Branch |  % Funcs |  % Lines |Uncovered Lines |
+--------------------------------|----------|----------|----------|----------|----------------|
+ contracts\                     |      100 |     87.5 |      100 |      100 |                |
+  MyERC20BidToken.sol           |      100 |      100 |      100 |      100 |                |
+  MyERC721NFT.sol               |      100 |      100 |      100 |      100 |                |
+  NFTDutchAuction_ERC20Bids.sol |      100 |    83.33 |      100 |      100 |                |
+--------------------------------|----------|----------|----------|----------|----------------|
+All files                       |      100 |     87.5 |      100 |      100 |                |
+--------------------------------|----------|----------|----------|----------|----------------|
+
+> Istanbul reports written to ./coverage/ and ./coverage.json
--- v3.0/test/Lock.ts
+++ v3.0/test/Lock.ts
@@ -3,7 +3,7 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Tests", function () {
+describe("Test Cases", function () {
   async function deployOneYearLockFixture() {
     
     const [owner, otherAccount, account3] = await ethers.getSigners();
@@ -18,31 +18,23 @@ describe("Tests", function () {
   }
 
   describe("Mint", function () {
-    it("ERC20 Tests - Max Supply Assertion", async function(){
+    it("ERC20 Tests - Cap Assertion", async function(){
         const {mintingTokenForERC20, owner, otherAccount} = await loadFixture(deployOneYearLockFixture);
         const maxSupply = await mintingTokenForERC20.cap();
-        // console.log(maxSupply);
-        // expect(await mintingTokenForERC20.getMaxSupply()).to.equal(500);
+        
 
         describe("ERC20 and ERC721 Tests", function(){
-            it("ERC20 by Owner", async function () {
+            it("ERC20 minting by Owner", async function () {
                 expect(await mintingTokenForERC20.mint(owner.address, 2000));
-
-                // const balanceOf = await mintingTokenForERC20.balanceOf(owner.address);
-                // console.log(balanceOf);
               });
             
-            it("ERC20 by Other Account", async function () {
+            it("ERC20 minting by Other Account", async function () {
             expect(await mintingTokenForERC20.mint(otherAccount.address, 2000));
-
-            // const balanceOf = await mintingTokenForERC20.balanceOf(otherAccount.address);
-            // console.log(balanceOf);
             });
 
-            it("ERC20 - Max Supply exceeded", async function () {
+            it("ERC20 - Cap exceeded", async function () {
                 await expect(mintingTokenForERC20.mint(otherAccount.address, 10005)).to.be.revertedWith('ERC20Capped: cap exceeded');
-                // const circulatingSupply = await mintingTokenForERC20.totalSupply();
-                // console.log(circulatingSupply);
+                
             });
 
             it("ERC 721 - should revert when maximum token supply is reached", async function () {
@@ -55,17 +47,17 @@ describe("Tests", function () {
             });
 
             describe("Auction Tests", function(){
-                it("ERC721 - Safe Mint - Owner", async function () {
+                it("ERC721 - Safe Mint by Owner", async function () {
                     const { mintingToken, owner } = await loadFixture(deployOneYearLockFixture);
                     expect(await mintingToken.safeMint(owner.address));
                 });
               
-                it("ERC721 - Safe Mint - Other Account", async function () {
+                it("ERC721 - Safe Mint by Other Account", async function () {
                     const { mintingToken, otherAccount } = await loadFixture(deployOneYearLockFixture);
                     await expect(mintingToken.connect(otherAccount).safeMint(otherAccount.address)).eventually.to.rejectedWith(Error, "VM Exception while processing transaction: reverted with reason string 'Ownable: caller is not the owner");
                 }); 
                 
-                it("Deploy Auction's Contract", async function () {
+                it("Deploy Dutch Auction Contract", async function () {
                     const { mintingTokenForERC20 ,mintingToken, owner, otherAccount } = await loadFixture(deployOneYearLockFixture);
                     expect(mintingToken.safeMint(owner.address));
                     
@@ -76,47 +68,42 @@ describe("Tests", function () {
 
                     describe("Bids", function(){
                     
-                        it("ERC20 Approval - Before", async function(){
+                        it("Pre ERC20 Approval", async function(){
                             await expect(nftDutchAuctionToken.connect(otherAccount).bid(2000)).to.be.revertedWith('Bidder has not approved enough tokens'); 
                         })
-                        it("ERC20 Approval Process", async function(){
+                        it("ERC20 Approved", async function(){
                             await mintingTokenForERC20.connect(otherAccount).approve(nftDutchAuctionToken.address, 2000);
                             
-                            describe("Post Approval Process", function(){
+                            describe("After approval", function(){
                                 
-                                it("Bid before ERC721 approval - Seller trying to bid", async function(){
+                                it("should revert when the seller is trying to bid", async function(){
                                     mintingTokenForERC20.mint(otherAccount.address, 2000);
-                                    // const otherAccountBal = await mintingTokenForERC20.balanceOf(otherAccount.address);
-                                    // console.log(otherAccountBal)
                                     await expect(nftDutchAuctionToken.connect(owner).bid(2000)).to.be.revertedWith('Sellers are not allowed to buy');
                                 })
 
-                                it("Bid before ERC721 approval", async function(){
+                                it("should revert when the token is not approved", async function(){
                                     mintingTokenForERC20.mint(otherAccount.address, 2000);
                                     mintingTokenForERC20.connect(otherAccount).approve(nftDutchAuctionToken.address, 2000)
-                                    // const otherAccountBal = await mintingTokenForERC20.balanceOf(otherAccount.address);
-                                    // console.log(otherAccountBal)
-                                    // await expect(nftDutchAuctionToken.connect(otherAccount).bid({value:2000})).to.be.revertedWith('ERC721: caller is not token owner or approved');
+                                   
                                     await expect(nftDutchAuctionToken.connect(otherAccount).bid(2000)).to.be.revertedWith('ERC721: caller is not token owner or approved');
                                 })
 
-                                it("Approving - Failure due to non-existent token id", async function(){
+                                it("should revert when the token id doesn't exist while approving", async function(){
                                     await expect(mintingToken.approve(nftDutchAuctionToken.address, 9)).to.be.revertedWith('ERC721: invalid token ID');
                                 });
                     
-                                it("Approval - Failure - Not the owner", async function () {
+                                it("should revert when the approve caller is not the token owner", async function () {
                                     await expect(mintingToken.connect(otherAccount).approve(nftDutchAuctionToken.address,1)).to.be.revertedWith('ERC721: approve caller is not token owner or approved for all');
                                 });
-                                it("Approving", async function () {
+                                it("Approved", async function () {
                                     
-                                    //const approvalResult = await mintingToken.approve(nftDutchAuctionToken.address, 0);
                                     expect(await mintingToken.approve(nftDutchAuctionToken.address,1));
-                                    describe("Bid - After Approval", function () {
-                                        it("Bid - Failure - Insufficient Funds", async function () {
+                                    describe("Bid After Approval", function () {
+                                        it("should rever when the bid placed is less than current price", async function () {
                                             await expect(nftDutchAuctionToken.connect(otherAccount).bid(200)).to.be.revertedWith('Bid value must be greater than or equal to the current price');
                                         });
                     
-                                        it("Bid - Success", async function () {
+                                        it("Successful Bid", async function () {
                                             await expect(nftDutchAuctionToken.connect(otherAccount).bid(2000));
                                             
                                             describe("Transfer to seller", function(){
@@ -132,17 +119,7 @@ describe("Tests", function () {
                                           await expect(nftDutchAuctionToken.connect(otherAccount).bid(2000 )).to.be.revertedWith("Auction already closed");
                                         });
 
-                                        // it("should close the auction after a successful bid", async function () {
-                                        //   await nftDutchAuctionToken.connect(bidder).bid(initialPrice);
-                                        //   const auctionEndStatus = await nftDutchAuctionToken.auctionEnd();
-                                        //   expect(auctionEndStatus).to.equal(true);
-                                        // });
-                    
-                                        
 
-                                        // it("Bid - Failure - Closed Auction", async function () {
-                                        //    await expect(nftDutchAuctionToken.connect(otherAccount).bid(410)).to.be.revertedWith('Auction is closed');;
-                                        // });
                                     });
                                     
                                 });
